e2ae24f660058281fc25beb212fda5ea66f9b6bf
Richard Neher <richard.neher@unibas.ch>
2023-07-10 15:37:43 +0200

    add forward in time evolution with density regulation

---
+++ src/density_regulation.py
@@ -0,0 +1,111 @@
+import numpy as np
+from heterogeneity import get_2d_hist
+
+def make_node(x,y,t,parent):
+    return {'children':[], 'x':x,'y':y, 'n_offspring':0, 'time':t, 'parent':parent}
+
+def calc_density(terminal_nodes, interaction_radius, Lx, Ly, bins_per_std = 5.0):
+    '''
+    generate an interpolation object that returns to local population density after
+    Gaussian smoothing with an interaction_radius. The function assumes Lx>=Ly.
+    '''
+    from scipy.ndimage import gaussian_filter
+    from scipy.interpolate import RegularGridInterpolator
+    nbins = int(np.ceil(bins_per_std/interaction_radius*Ly))
+    H, bx, by = get_2d_hist(terminal_nodes, Lx, Ly, nbins)
+    dx = bx[1]-bx[0]
+
+    dens = gaussian_filter(H, interaction_radius/dx, mode='wrap')
+    dens *= 1.0/dx**2/dens.sum()*len(terminal_nodes)
+
+    return RegularGridInterpolator(((bx[1:]+bx[:-1])*0.5, (by[1:]+by[:-1])*0.5), dens,
+                                   method='linear', bounds_error=False, fill_value=None)
+
+def evolve(terminals, t, Lx=1, Ly=1, D=0.1, target_density = 100.0, density_reg = 0.1,
+           interaction_radius = 0.1, global_pop_reg=True):
+    '''
+    step the population forward. The population is a list of terminal nodes
+    that generates a number of offspring dependent on local density.
+    '''
+
+    # calculate local density at for each extant individual
+    density = calc_density(terminals, interaction_radius, Lx, Ly)
+    dens_array = density([(c['x']%Lx, c['y']%Ly) for c in terminals])
+
+    # calculate fitness of each extant individual
+    fitness = np.maximum(0.1,1 + density_reg*(1-dens_array/target_density))
+    #print(fitness.mean(), dens_array.mean(), target_density, len(terminals))
+    if global_pop_reg: # add global density regulation (set average fitness to one, add density independent term)
+        fitness += (1 - fitness.mean()) + 0.1*(1-len(terminals)/(target_density*Lx*Ly))
+
+    # determine offspring number and generate new population
+    offspring = np.random.poisson(np.maximum(0.001,fitness))
+    new_terminals = []
+    diff_std = np.sqrt(2*D)
+    for noff, parent in zip(offspring, terminals):
+        for c in range(noff):
+            dx,dy = np.random.randn(2)*diff_std
+            parent['children'].append(make_node(parent['x']+dx, parent['y']+dy, t, parent))
+        new_terminals.extend(parent['children'])
+
+    # prune branches that didn't yield any offspring.
+    for n in terminals:
+        if n['children']: continue
+        x = n
+        while len(x['children'])==0:
+            x['parent']['children'] = [c for c in x['parent']['children'] if c!=x]
+            x = x['parent']
+
+    return new_terminals
+
+def dict_to_phylo_tree(d):
+    '''
+    Convert custom tree to a Biopython tree. Very slow due to many trivial nodes.
+    '''
+    from Bio.Phylo.BaseTree import Clade, Tree
+
+    def clade_from_dict(d, parent=None):
+        clade = Clade()
+        clade.name = 'root'
+        clade.branch_length = d['time'] - parent.t if parent else 0.001
+        clade.t = d['time']
+        clade.pos = [d['x'], d['y']]
+        return clade
+
+    def add_clades(clade, d):
+        for k in d['children']:
+            new_clade = clade_from_dict(k, clade)
+            clade.clades.append(new_clade)
+            add_clades(new_clade, k)
+
+    tree = Tree()
+    tree.root = clade_from_dict(d, None)
+    add_clades(tree.root,d)
+
+    return tree
+
+
+if __name__=="__main__":
+    N = 2000
+    L = 1
+    D = 0.1*L**2/N
+    tree = make_node(L/2,L/2,-2, None)
+    tree['children'] = [make_node(np.random.random()*3*L, np.random.random()*L, -1, tree) for i in range(N)]
+    terminal_nodes = tree['children']
+    interaction_radius = 0.2
+    density = calc_density(terminal_nodes, interaction_radius, 3*L, L)
+    density_reg = 0.2
+    print(f"starting density: {float(density([0,0]).squeeze()):1.2f}")
+    for t in range(3*N):
+        terminal_nodes = evolve(terminal_nodes, t, Lx=3*L, Ly=L, interaction_radius=interaction_radius,
+                                density_reg=density_reg, D=D, target_density=N/3, global_pop_reg=False)
+        if t%(N/5)==0: print(t, len(terminal_nodes))
+    T = dict_to_phylo_tree(tree)
+    from Bio import Phylo
+    Phylo.draw(T)
+
+    from heterogeneity import get_2d_hist
+    import matplotlib.pyplot as plt
+    H, bx, by = get_2d_hist(terminal_nodes, 3*L, L, 20)
+    plt.figure()
+    plt.matshow(H)
--- src/heterogeneity.py
+++ src/heterogeneity.py
@@ -2,9 +2,11 @@ from free_diffusion import *
 import matplotlib.pyplot as plt
 
 def get_2d_hist(terminal_nodes, Lx, Ly, bins):
-    x_pos = (np.array([c['x'] for c in terminal_nodes])+Lx/2)%Lx
-    y_pos = (np.array([c['y'] for c in terminal_nodes])+Ly/2)%Ly
-    return np.histogram2d(x_pos, y_pos, bins=(np.linspace(0,Lx,bins+1),np.linspace(0,Ly,bins+1)))
+    x_pos = (np.array([c['x'] for c in terminal_nodes]))%Lx
+    y_pos = (np.array([c['y'] for c in terminal_nodes]))%Ly
+    assert Ly<=Lx
+    aspect=Lx/Ly
+    return np.histogram2d(x_pos, y_pos, bins=(np.linspace(0,Lx,int(aspect*bins+1)),np.linspace(0,Ly,bins+1)))
 
 def get_granularity(ntips, D, yule=False, L=1, bins=10):
     T = random_tree(ntips, yule=yule)
@@ -29,10 +31,12 @@ if __name__=="__main__":
     area = np.minimum(1,D_array*np.pi**2)
     pred_heterogeneity = np.sqrt(1/area - 1)  # p*(1/p - 1)**2 + (1-p) = p*(1/p^2 - 2/p + 1) + (1-p) = 1/p - 2 + p -1 + p = 1/p - 1
 
+    plt.figure()
     plt.plot(D_array, res_mean, label='free diffusion')
     plt.plot(D_array, np.ones_like(D_array)*np.sqrt(nbins/ntips), label='well mixed limit')
     #plt.plot(D_array, pred_heterogeneity, label='well mixed limit')
     plt.xscale('log')
-    plt.xlabel('diffusion constant')
+    plt.xlabel('diffusion constant [habitat_size^2/T_c]')
     plt.ylabel('heterogeneity')
+    plt.legend()
     plt.savefig('figures/heterogeneity_free_diffusion.png')
