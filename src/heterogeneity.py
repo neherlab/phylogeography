e3b76e8aae24ee8ee9663f62a6e258302eef6fcf
Richard Neher <richard.neher@unibas.ch>
2023-08-06 12:30:34 +0200

    refactor

--- src/heterogeneity.py
+++ src/heterogeneity.py
@@ -18,22 +18,25 @@ def get_granularity(ntips, D, yule=False, L=1, bins=10):
 if __name__=="__main__":
     n_iter = 10
     ntips = 1000
-    linear_bins=5
-    res = []
     D_array = np.logspace(-2,2,101)
-    for D in D_array:
-        res.append([get_granularity(ntips, D, bins=linear_bins) for i in range(n_iter)])
-
-    res = np.array(res)
-    res_mean = res.mean(axis=1)
-
-    nbins=linear_bins**2
-    area = np.minimum(1,D_array*np.pi**2)
-    pred_heterogeneity = np.sqrt(1/area - 1)  # p*(1/p - 1)**2 + (1-p) = p*(1/p^2 - 2/p + 1) + (1-p) = 1/p - 2 + p -1 + p = 1/p - 1
 
     plt.figure()
-    plt.plot(D_array, res_mean, label='free diffusion')
-    plt.plot(D_array, np.ones_like(D_array)*np.sqrt(nbins/ntips), label='well mixed limit')
+
+
+    for line_style, linear_bins in zip(['-', '--', '-.'], [3,5,10]):
+        nbins=linear_bins**2
+        res = []
+        for D in D_array:
+            res.append([get_granularity(ntips, D, bins=linear_bins) for i in range(n_iter)])
+
+        res = np.array(res)
+        res_mean = res.mean(axis=1)
+        plt.plot(D_array, res_mean, label=f'free diffusion, dx=L/{linear_bins}', c='C0', ls=line_style)
+        plt.plot(D_array, np.ones_like(D_array)*np.sqrt(nbins/ntips), label=f'well mixed limit dx=L/{linear_bins}', c='C1', ls=line_style)
+
+    # area = np.minimum(1,D_array*np.pi**2)
+    # pred_heterogeneity = np.sqrt(1/area - 1)  # p*(1/p - 1)**2 + (1-p) = p*(1/p^2 - 2/p + 1) + (1-p) = 1/p - 2 + p -1 + p = 1/p - 1
+
     #plt.plot(D_array, pred_heterogeneity, label='well mixed limit')
     plt.xscale('log')
     plt.xlabel('diffusion constant [habitat_size^2/T_c]')
--- src/inflated_diffusion.py
+++ src/inflated_diffusion.py
@@ -5,72 +5,46 @@ from heterogeneity import get_2d_hist, get_granularity
 from estimate_diffusion_from_tree import estimate_diffusion
 import matplotlib.pyplot as plt
 
+def estimate_inflated_diffusion(D, interaction_radius, density_reg, N, Lx=1, Ly=1, linear_bins=5, n_iter=10):
+    # set up tree and initial population uniformly in space
+    tree = make_node(Lx/2,Ly/2,-2, None)
+    tree['children'] = [make_node(np.random.random()*Lx, np.random.random()*Ly, -1, tree)
+                        for i in range(N)]
+    terminal_nodes = tree['children']
+
+    density_variation = []
+    D_est = []
+    for t in range((n_iter+2)*N):
+        terminal_nodes = evolve(terminal_nodes, t, Lx=Lx, Ly=Ly, interaction_radius=interaction_radius,
+                                density_reg=density_reg, D=D, target_density=N)
+        if t%(N//5)==0 and t>2*N: # take samples after burnin every 5 Tc
+            H, bx, by = get_2d_hist(terminal_nodes, Lx, Ly, linear_bins)
+            density_variation.append(np.std(H)/N*np.prod(H.shape))
+            D_res = estimate_diffusion(tree)
+            D_est.extend([D_res['Dx_total'], D_res['Dy_total']])
+
+    return {"density_variation": density_variation, "D_est": D_est}
+
 if __name__=="__main__":
     N = 500
     Lx, Ly = 1, 1
     res_density = {}
     res_density_mean = {}
-    D_est = {}
+    D_est = []
     D_array_dens = np.logspace(-2,0,6)*Lx*Ly*2/N
     n_iter = 10
     linear_bins=5
-    for interaction_radius, density_reg in product([0.02, 0.05, 0.1, 0.5, 2], [0.05, 0.1, 0.2]):
+    iR, dR = [0.02, 0.05, 0.1, 0.5, 2], [0.05, 0.1, 0.2]
+    iR, dR = [0.02], [0.05]
+    for interaction_radius, density_reg in product(iR, dR):
         print(f"{interaction_radius=:1.3f}, {density_reg=:1.3f}")
-        label = (interaction_radius, density_reg)
-        tmp_density = []
-        tmp_D_est = []
         for D in D_array_dens:
-            tree = make_node(Lx/2,Ly/2,-2, None)
-            tree['children'] = [make_node(np.random.random()*Lx, np.random.random()*Ly, -1, tree)
-                                for i in range(N)]
-            terminal_nodes = tree['children']
-            tmp_density_local = []
-            tmp_D_est_local = []
-            for t in range((n_iter+2)*N):
-                terminal_nodes = evolve(terminal_nodes, t, Lx=Lx, Ly=Ly, interaction_radius=interaction_radius,
-                                        density_reg=density_reg, D=D, target_density=N)
-                if t%(N)==0 and t>2*N:
-                    H, bx, by = get_2d_hist(terminal_nodes, Lx, Ly, linear_bins)
-                    tmp_density_local.append(np.std(H)/N*np.prod(H.shape))
-                    D_res = estimate_diffusion(tree)
-                    tmp_D_est_local.extend([D_res['Dx_total'], D_res['Dy_total']])
-            tmp_density.append(tmp_density_local)
-            tmp_D_est.append(np.mean(tmp_D_est_local, axis=0))
-        res_density[label] = np.array(tmp_density)
-        res_density_mean[label] = res_density[label].mean(axis=1)
-        D_est[label] = np.array(tmp_D_est)
+            res = estimate_inflated_diffusion(D, interaction_radius, density_reg, N, Lx=Lx, Ly=Ly, linear_bins=linear_bins, n_iter=n_iter)
+            Dx, Dy = np.mean(res["D_est"], axis=0)
+            stdDx, stdDy = np.std(res["D_est"], axis=0)
+            D_est.append({"interaction_radius":interaction_radius, "density_reg": density_reg,
+                              "D":D, "meanD_x": Dx, "meanD_y": Dx, "stdDx": stdDx, "stdDy": stdDy, "density_variation": res['density_variation'].mean()})
 
+    import pandas as pd
+    pd.DataFrame(D_est).to_csv('data/inflated_diffusion_{N=}.csv')
 
-    nbins=linear_bins**2
-    res = []
-    D_array = np.logspace(-2,2,101)
-    for D in D_array:
-        res.append([get_granularity(N, D, bins=linear_bins) for i in range(n_iter)])
-
-    res = np.array(res)
-    res_mean = res.mean(axis=1)
-
-    area = np.minimum(1,D_array*np.pi**2)
-
-    ls = ['-', '-.', "--"]
-    for i, dr in enumerate(res_density_mean):
-        plt.plot(D_array_dens*N/Lx/Ly, res_density_mean[dr],
-                 label=f'r={dr[0]}, a={dr[1]}', ls=ls[i%len(ls)], c=f"C{i//len(ls)}")
-    plt.plot(D_array, res_mean, label='diffusion')
-    plt.plot(D_array, np.ones_like(D_array)*np.sqrt(nbins/N), label='well mixed limit')
-    plt.xscale('log')
-    plt.xlabel('diffusion constant')
-    plt.ylabel('heterogeneity')
-    plt.legend()
-
-    plt.figure()
-    for i, dr in enumerate(res_density_mean):
-        plt.plot(D_array_dens*N, D_est[dr]/D_array_dens,
-                 label=f'r={dr[0]}, a={dr[1]}', ls=ls[i%len(ls)], c=f"C{i//len(ls)}")
-
-    plt.legend()
-    plt.plot(N*D_array_dens, np.ones_like(D_array_dens), c='k')
-    plt.xlabel('true N*D')
-    plt.xlabel('estimated N*D')
-    plt.yscale('log')
-    plt.xscale('log')
